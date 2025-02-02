fdd82f13bde2f3f6c3362937d1be4f43bda771fe
pavankunasani <pavankunasani@users.noreply.github.com>
2025-02-02 23:39:26 +0530

    Add files via upload

--- planets/src/main/java/org/earth/resource/FilmResource.java
+++ planets/src/main/java/org/earth/resource/FilmResource.java
@@ -10,6 +10,7 @@ import org.earth.repository.FilmRepository;
 import org.jboss.logging.annotations.Param;
 
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Path("/api/v1/films")
 public class FilmResource {
@@ -31,4 +32,45 @@ public class FilmResource {
         Optional<Film> filmInfo = filmRepository.getFilm(filmId);
         return filmInfo.isPresent() ? filmInfo.get().getTitle() : "No Film was found";
     }
+
+    @GET
+    @Path("/film/{page}/{minLength}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getPagedData(long page,short minLength){
+        return filmRepository.getFilmsPaged(page,minLength).stream().map(f -> String.format("%s (%d min)", f.getTitle(), f.getLength()))
+                .collect(Collectors.joining("\n"));
+    }
+
+    @GET
+    @Path("/actors/{startsWith}/{minLength}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String actors(String startsWith, short minLength) {
+        return filmRepository.actors(startsWith, minLength)
+                .map(f -> String.format("%s (%d min): %s",
+                        f.getTitle(),
+                        f.getLength(),
+                        f.getActors().stream()
+                                .map(a -> String.format("%s %s", a.getFirstName(), a.getLastName()))
+                                .collect(Collectors.joining(", "))))
+                .collect(Collectors.joining("\n"));
+    }
+
+    @GET
+    @Path("/update/{minLength}/{rentalRate}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String update(short minLength, Float rentalRate) {
+        filmRepository.updateRentalRate(minLength, rentalRate);
+        return filmRepository.getFilms(minLength)
+                .map(f -> String.format("%s (%d min) - $%f", f.getTitle(), f.getLength(), f.getRentalRate()))
+                .collect(Collectors.joining("\n"));
+    }
+
+    @GET
+    @Path("/film/json/{filmId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Film getFilmJsonView(short filmId){
+        Optional<Film> filmInfo = filmRepository.getFilm(filmId);
+        return filmInfo.orElse(null);
+    }
+
 }
